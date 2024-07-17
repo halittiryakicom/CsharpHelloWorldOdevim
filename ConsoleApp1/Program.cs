f1032d76ccad20c4e899adc365df8308b826b720
Halit TİRYAKİ <halittiryaki61@outlook.com>
2024-07-17 13:09:02 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halit TİRYAKİ");
+            Console.WriteLine("Patika'nın içeriğine uzman kadrosuna güvendiğimden, yazılım dünyasına hızlı bir şekilde giriş yapabileceğim için buradayım.");
+        }
+        
+    }
+}
